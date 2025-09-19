72a21d0b32809bd7ba2f47f7819323b9e25e14a9
Antony Omar <omarestuvoenmachupichu@gmail.com>
2025-09-19 03:23:36 -0500

    Añadir funcionar mostrar amigos

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-function agregarAmigo(nombre){
+function agregarAmigo(){
     //capturar el valor del input
-    nombre = document.getElementById("amigo").value;
+    let nombre = document.getElementById("amigo").value;
     //validar entrada
     if(nombre === ""){
         alert("Por favor ingresa un nombre");
@@ -11,5 +11,16 @@ function agregarAmigo(nombre){
     }else{
         amigos.push(nombre);
         document.getElementById("amigo").value = "";
+        mostrarAmigos();
+    }
+}
+
+function mostrarAmigos(){
+    let listaResultado = document.getElementById("resultado");
+    listaResultado.innerHTML = "";
+    for(let i = 0; i < amigos.length; i++){
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        listaResultado.appendChild(li);
     }
 }
