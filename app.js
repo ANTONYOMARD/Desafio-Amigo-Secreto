1bde74b67a7e2e403cb78d818e97e3d5cfe56e97
Antony Omar <omarestuvoenmachupichu@gmail.com>
2025-09-19 03:51:16 -0500

    Añadir funcionar sortear amigos

--- app.js
+++ app.js
@@ -16,11 +16,25 @@ function agregarAmigo(){
 }
 
 function mostrarAmigos(){
-    let listaResultado = document.getElementById("resultado");
+    //obtener la referencia de la lista y vaciarla
+    let listaResultado = document.getElementById("listaAmigos");
     listaResultado.innerHTML = "";
+    //recorrer el array y crear elementos li
     for(let i = 0; i < amigos.length; i++){
         let li = document.createElement("li");
         li.textContent = amigos[i];
         listaResultado.appendChild(li);
     }
+}
+
+function sortearAmigo(){
+    //validar que haya amigos añadidos y haya menos 2 amigos
+    if(amigos.length < 0 || amigos.length < 2){
+        alert("Por favor añade al menos 2 amigos para sortear");
+    }
+    //generar un indice aleatorio
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    //mostrar el resultado
+    let resultado = amigos[indiceAleatorio];
+    document.getElementById("resultado").innerText = `Tu amigo secreto es: ${resultado}`;
 }
